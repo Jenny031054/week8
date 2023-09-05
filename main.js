7fb870e8dcd1aed0afc12eca2934a09ceda36a75
林炘秀 <jenny031054@gmail.com>
2023-09-05 18:05:40 +0800

    9/5-update

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 24,
+    slidesPerView: 3,
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+    mousewheel: true,
+  });
